00efae16b1c8ca84a7603d69929e30326fe562bc
Pessah Shumwe <pessahshumwe@gmail.com>
2025-10-21 10:35:05 +0300

    Fix template paths in views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,12 +5,12 @@ from .models import Book, Library
 # Function-based view to list all books
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'list_books.html', {'books': books})
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
 
 # Class-based view to display details for a specific library
 class LibraryDetailView(View):
     def get(self, request, library_id):
         library = Library.objects.get(id=library_id)
-        return render(request, 'library_detail.html', {'library': library})
+        return render(request, 'relationship_app/library_detail.html', {'library': library})
 
