03e8eb7bf4410e487fff1bdaa4265891e06747e5
Pessah Shumwe <pessahshumwe@gmail.com>
2025-10-21 03:39:06 +0300

    Fix query_samples.py to include Librarian.objects.get(library=...)

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,17 +1,15 @@
 from .models import Author, Book, Library, Librarian
 
-# Query all books by a specific author
-def books_by_author(author_name):
+def get_books_by_author(author_name):
     author = Author.objects.get(name=author_name)
     return Book.objects.filter(author=author)
 
-# List all books in a library
-def books_in_library(library_name):
+def get_books_in_library(library_name):
     library = Library.objects.get(name=library_name)
-    return library.books.all()
+    lib = Library.objects.get(name=library_name)
+    return lib.books.all()
 
-# Retrieve the librarian for a library
-def librarian_for_library(library_name):
-    library = Library.objects.get(name=library_name)
-    return Librarian.objects.get(library=library)
+def get_librarian_for_library(library_name):
+    lib = Library.objects.get(name=library_name)
+    return Librarian.objects.get(library=lib)
 
