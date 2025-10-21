eed23ba5b4686ea731f409c015f507814cb9b4a3
Pessah Shumwe <pessahshumwe@gmail.com>
2025-10-21 03:16:29 +0300

    Implemented function-based and class-based views with URL configuration in relationship_app

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -18,5 +18,16 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
+
+        path('admin/', admin.site.urls)
+
+        ]
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('relationship_app.urls')),
 ]
+
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views import View
+from .models import Book, Library
 
-# Create your views here.
+# Function-based view to list all books
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'list_books.html', {'books': books})
+
+
+# Class-based view to display details for a specific library
+class LibraryDetailView(View):
+    def get(self, request, library_id):
+        library = Library.objects.get(id=library_id)
+        return render(request, 'library_detail.html', {'library': library})
+
